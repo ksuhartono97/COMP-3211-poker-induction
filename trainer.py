03b69c74ff05da000f02edb6fad00e174b30cff3
Kristian Suhartono <ksuhartono@connect.ust.hk>
2017-11-06 00:50:47 +0800

    trying pandas and oversampling

--- trainer.py
+++ trainer.py
@@ -1,5 +1,12 @@
 import csv
 import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import recall_score
+from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import RandomOverSampler
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
 
@@ -13,6 +20,41 @@ resultDataMLP = []
 ctr = 0
 
 handArrCount = [0,0,0,0,0,0,0,0,0,0]
+
+trainSet  = pd.read_csv('train.csv')
+# print(trainSet.iloc[0])
+
+model_variables = ['S1', 'C1', 'S2', 'C2', 'S3', 'C3',
+                   'S4', 'C4', 'S5', 'C5', 'hand']
+
+trainingDataRelevant = trainSet[model_variables]
+
+trainingEncoded = pd.get_dummies(trainingDataRelevant)
+
+trainingFeatures, testFeatures, \
+    trainingTarget, testTarget = train_test_split(trainingEncoded.drop(['hand'], axis=1),
+                                                  trainingEncoded['hand'],
+                                                  test_size=.1,
+                                                  random_state=12)
+
+xTrain, xVal, yTrain, yVal = train_test_split(trainingFeatures, trainingTarget,
+                                              test_size= .1,
+                                              random_state=12)
+
+# sm = SMOTE(random_state=12, ratio="minority")
+sm = RandomOverSampler(random_state=12, ratio="minority")
+
+xTrainRes, yTrainRes = sm.fit_sample(xTrain, yTrain)
+
+clfRf = RandomForestClassifier(n_estimators=25, random_state=12)
+clfRf.fit(xTrainRes, yTrainRes)
+
+print('Validation results')
+print(clfRf.score(xVal, yVal))
+# print(recall_score(yVal, clfRf.predict(xVal)))
+print('\nTest Results')
+print(clfRf.score(testFeatures, testTarget))
+# print(recall_score(testTarget, clfRf.predict(testFeatures)))
 
 with open('train.csv', 'rb') as csvfile:
     trainReadResult = csv.reader(csvfile, delimiter=' ', quotechar='|')
@@ -54,10 +96,58 @@ lr.fit(X,y)
 clf = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
 clf.fit(X,y)
 
-print handArrCount
+# print handArrCount
 
-resultDataLR = lr.predict(testX)
-resultDataMLP = clf.predict(testX)
+rf = RandomForestClassifier(n_estimators=25, random_state=12)
+rf.fit(X, y)
+print "Random forest, no oversampling"
+print rf.score(testX, testY)
 
-print lr.score(testX, testY)
-print clf.score(testX, testY)
+rf.fit(xTrainRes, yTrainRes)
+print "Random forest, oversampling"
+print rf.score(testX, testY)
+
+resultDataRF = rf.predict(testX)
+
+print resultDataRF
+
+resultPanda = pd.DataFrame(data=resultDataRF, columns=['hand'])
+print resultPanda.hand.value_counts()
+
+predictionX = []
+
+with open('test.csv', 'rb') as csvfile:
+    testReader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    next(testReader);
+    for row in testReader:
+        splitted = row[0].split(',')
+        handList = splitted[1:]
+        predictionX.append(np.array(handList))
+        # testY.append(int(splitted[-1]))
+
+resultDataRF = rf.predict(predictionX)
+
+print "Test File Results"
+print resultDataRF
+
+resultPanda = pd.DataFrame(data=resultDataRF, columns=['hand'])
+print resultPanda.hand.value_counts()
+resultPanda.index += 1
+resultPanda.to_csv("submission.csv", index_label='id', columns=['hand'])
+
+# resultDataLR = lr.predict(testX)
+# resultDataMLP = clf.predict(testX)
+#
+# print "Logistic Regression, no oversampling"
+# print lr.score(testX, testY)
+#
+# print "Logistic Regression, oversampling"
+# lr.fit(xTrainRes, yTrainRes)
+# print lr.score(testX, testY)
+#
+# print "Multi Layer Perceptron, no oversampling"
+# print clf.score(testX, testY)
+#
+# print "Multi Layer Perceptron, oversampling"
+# clf.fit(xTrainRes, yTrainRes)
+# print clf.score(testX, testY)
